f3cb0081db2da01b7569c22c4cb3e8c084caf0f8
Brandon <brndnwood@gmail.com>
2022-06-23 10:09:42 -0500

    another commit

---
+++ Sources/MeterSwiftUI/MeterSwiftUI.swift
@@ -0,0 +1,6 @@
+public struct MeterSwiftUI {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/MeterSwiftUITests/MeterSwiftUITests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MeterSwiftUI
+
+final class MeterSwiftUITests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MeterSwiftUI().text, "Hello, World!")
+    }
+}
