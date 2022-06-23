eff77c85276a6a4ca58f1182b69bf66b6a97a0e1
Brandon <brndnwood@gmail.com>
2022-06-23 11:11:21 -0500

    updates

--- Sources/MeterSwiftUI/MeterSwiftUI.swift
+++ Sources/MeterSwiftUI/MeterSwiftUI.swift
@@ -1,6 +1,21 @@
-public struct MeterSwiftUI {
+import SwiftUI
+
+@available(iOS 15, *)
+public struct MeterSwiftUI : View {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+
+struct MeterSwiftUI_Previews: PreviewProvider {
+    static var previews: some View {
+        MeterSwiftUI()
     }
 }
